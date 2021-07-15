8a16c58eb3d3c26abc8679fd5476c440c47408c0
Rohit Arora <arora22rohit@live.com>
2021-07-15 15:36:29 +0530

    Update

---
+++ AgoraCustomPlugin/Assets/Scripts/VideoStreaming/StreamManager.cs
@@ -0,0 +1,224 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+#if (UNITY_2018_3_OR_NEWER && UNITY_ANDROID)
+using System.Collections;
+using UnityEngine.Android;
+#endif
+using agora_gaming_rtc;
+
+public enum TestSceneEnum
+{
+    AppScreenShare,
+    DesktopScreenShare,
+    _Template,
+    InjectStream,
+    One2One
+};
+
+/// <summary>
+///    TestHome serves a game controller object for this application.
+/// </summary>
+public class StreamManager : MonoBehaviour
+{
+
+    // Use this for initialization
+#if (UNITY_2018_3_OR_NEWER && UNITY_ANDROID)
+    private ArrayList permissionList = new ArrayList();
+#endif
+    static IVideoChatClient app = null;
+
+    // PLEASE KEEP THIS App ID IN SAFE PLACE
+    // Get your own App ID at https://dashboard.agora.io/
+    
+    private string AppID = "0736881ee591479c978731a9c0712207";
+
+    [Header("UI Controls")]
+
+    private bool _initialized = false;
+
+    void Awake()
+    {
+#if (UNITY_2018_3_OR_NEWER && UNITY_ANDROID)
+		permissionList.Add(Permission.Microphone);         
+		permissionList.Add(Permission.Camera);               
+#endif
+
+        // keep this alive across scenes
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        Debug.Log("Screen Dimension: " + Screen.width + " Height: " + Screen.height);
+        CheckAppId();
+        LoadLastChannel();
+        ShowVersion();
+        InitializeApp();
+    }
+
+    void Update()
+    {
+        CheckPermissions();
+        CheckExit();
+    }
+
+    private void CheckAppId()
+    {
+        Debug.Assert(AppID.Length > 10, "Please fill in your AppId first on Game Controller object.");
+        if (AppID.Length > 10) { _initialized = true; }
+        GameObject go = GameObject.Find("AppIDText");
+        if (_initialized && go != null)
+        {
+            Text appIDText = go.GetComponent<Text>();
+            appIDText.text = "AppID:" + AppID.Substring(0, 4) + "********" + AppID.Substring(AppID.Length - 4, 4);
+        }
+    }
+
+    /// <summary>
+    ///   Checks for platform dependent permissions.
+    /// </summary>
+    private void CheckPermissions()
+    {
+#if (UNITY_2018_3_OR_NEWER && UNITY_ANDROID)
+        foreach(string permission in permissionList)
+        {
+            if (!Permission.HasUserAuthorizedPermission(permission))
+            {                 
+				Permission.RequestUserPermission(permission);
+			}
+        }
+#endif
+    }
+
+
+    private void LoadLastChannel()
+    {
+        string channel = PlayerPrefs.GetString("ChannelName");
+        if (!string.IsNullOrEmpty(channel))
+        {
+            GameObject go = GameObject.Find("ChannelName");
+            InputField field = go.GetComponent<InputField>();
+
+            field.text = channel;
+        }
+    }
+
+    
+    public void InitializeApp()
+    {
+        string channelName = "Youtube";
+
+
+        if (!_initialized)
+        {
+            Debug.LogError("AppID null or app is not initialized properly!");
+            return;
+        }
+
+        app = new TranscodingApp();
+
+        if (app == null)
+        {
+            Debug.LogError("App is Null");
+            return;
+        }
+
+        // load engine
+        app.LoadEngine(AppID);
+        // join channel and jump to next scene
+        app.Join(channelName);
+        OnLevelFinishedLoading(); // configure GameObject after scene is loaded
+        
+    }
+
+    void ShowVersion()
+    {
+        GameObject go = GameObject.Find("VersionText");
+        if (go != null)
+        {
+            Text text = go.GetComponent<Text>();
+            var engine = IRtcEngine.GetEngine(AppID);
+            Debug.Assert(engine != null, "Failed to get engine, appid = " + AppID);
+            text.text = IRtcEngine.GetSdkVersion();
+            Debug.Log("SDK Version: "+ IRtcEngine.GetSdkVersion());
+        }
+    }
+
+    bool _previewing = false;
+    
+
+    public void OnLevelFinishedLoading()
+    {
+        // Stop preview
+        if (_previewing)
+        {
+            var engine = IRtcEngine.QueryEngine();
+            if (engine != null)
+            {
+                engine.StopPreview();
+                _previewing = false;
+            }
+        }
+
+        if (!ReferenceEquals(app, null))
+        {
+            app.OnSceneLoaded(); // call this after scene is loaded
+        }
+    }
+
+    void OnApplicationPause(bool paused)
+    {
+        if (!ReferenceEquals(app, null))
+        {
+            app.EnableVideo(paused);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("OnApplicationQuit, clean up...");
+        if (_previewing)
+        {
+            var engine = IRtcEngine.QueryEngine();
+            if (engine != null)
+            {
+                engine.StopPreview();
+                _previewing = false;
+            }
+        }
+        if (!ReferenceEquals(app, null))
+        {
+            app.UnloadEngine();
+        }
+        IRtcEngine.Destroy();
+    }
+
+    void CheckExit()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // Gracefully quit on OS like Android, so OnApplicationQuit() is called
+            Application.Quit();
+#endif
+        }
+    }
+
+    /// <summary>
+    ///   This method shows the CheckVideoDeviceCount API call.  It should only be used
+    //  after EnableVideo() call.
+    /// </summary>
+    /// <param name="engine">Video Engine </param>
+    void CheckDevices(IRtcEngine engine)
+    {
+        VideoDeviceManager deviceManager = VideoDeviceManager.GetInstance(engine);
+        deviceManager.CreateAVideoDeviceManager();
+
+        int cnt = deviceManager.GetVideoDeviceCount();
+        Debug.Log("Device count =============== " + cnt);
+    }
+}
